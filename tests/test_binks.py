2c5b434aa51b4ea597badc23c0fc43942751dbd1
xdd1874 <bepox0531@gmail.com>
2019-08-13 16:42:33 +0800

    removing some codes

--- tests/test_binks.py
+++ tests/test_binks.py
@@ -6,9 +6,6 @@
 import pytest
 
 
-from binks import binks
-
-
 @pytest.fixture
 def response():
     """Sample pytest fixture.
@@ -17,9 +14,11 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    pass
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    pass
